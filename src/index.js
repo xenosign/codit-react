bfc99a2d6b5d25a17641d3ebe85b8a9d94b2e529
kdtTetz <xenosign@naver.com>
2023-11-05 11:22:27 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 
+const WINS = ["scissor", "paper", "rock"];
+
+function getResult(right) {
+  const left = Math.floor(Math.random() * 3);
+  if (WINS[left] === right) return console.log("무승부");
+  else if (WINS[left] === "scissor") {
+    if (right === "paper") return console.log("승리");
+    return console.log("패배");
+  } else if (WINS[left] === "paper") {
+    if (right === "rock") return console.log("승리");
+    return console.log("패배");
+  } else if (WINS[left] === "rock") {
+    if (right === "scissor") return console.log("승리");
+    return console.log("패배");
+  }
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
     <h1>가위바위보</h1>
-    <button>가위</button>
-    <button>바위</button>
-    <button>보</button>
+    <button onClick={() => getResult("scissor")}>가위</button>
+    <button onClick={() => getResult("rock")}>바위</button>
+    <button onClick={() => getResult("paper")}>보</button>
   </>
 );
